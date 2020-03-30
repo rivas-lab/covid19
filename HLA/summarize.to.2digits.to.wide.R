9a07738b438a23de5afbdaa700915ccbd042a71a
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-03-30 11:11:53 -0700

    update tables

--- HLA/summarize.to.2digits.to.wide.R
+++ HLA/summarize.to.2digits.to.wide.R
@@ -8,7 +8,7 @@ suppressPackageStartupMessages(library(data.table))
 
 ####################################################################
 in_f  <- args[1]
-out_f <- args[2]
+out_d <- args[2]
 ####################################################################
 
 df <- fread(in_f)
@@ -17,19 +17,38 @@ if(!'Count' %in% colnames(df)){
     df$Count <- df$AlleleCount
 }
 
-df %>%
-rename(!!str_replace(names(df)[1], '#', '') := names(df)[1] ) %>%
-rename(!!'group' := names(df)[3] ) %>%
-select(Allele, group, Count) %>%
-spread(Allele, Count, fill=0) %>%
-left_join(    
+# genes <- c('HLA-A',
+# 'HLA-B',
+# 'HLA-C',
+# 'HLA-DPA1',
+# 'HLA-DPB1',
+# 'HLA-DQA1',
+# 'HLA-DQB1',
+# 'HLA-DRB1',
+# 'HLA-DRB3',
+# 'HLA-DRB4',
+# 'HLA-DRB5')
+genes <- df %>% pull(names(df)[1]) %>% unique()
+
+for(gene in genes){
+    if (!file.exists(file.path(out_d, gene))){
+        dir.create(file.path(out_d, gene))
+    }
     df %>%
     rename(!!str_replace(names(df)[1], '#', '') := names(df)[1] ) %>%
+    filter(Locus == gene) %>%
     rename(!!'group' := names(df)[3] ) %>%
-    group_by(Locus, group) %>%
-    summarise(n = sum(Count)) %>%
-    spread(Locus, n, fill=0),
-    by='group'
-) %>%
-rename(!!sprintf('#%s', names(df)[3]) := 'group') %>%
-fwrite(out_f, sep='\t', na = "NA", quote=F)
+    select(Allele, group, Count) %>%
+    spread(Allele, Count, fill=0) %>%
+    left_join(    
+        df %>%
+        rename(!!str_replace(names(df)[1], '#', '') := names(df)[1] ) %>%
+        rename(!!'group' := names(df)[3] ) %>%
+        filter(Locus == gene) %>%
+        group_by(group) %>%
+        summarise(n = sum(Count)),
+        by='group'
+    ) %>%
+    rename(!!sprintf('#%s', names(df)[3]) := 'group') %>%
+    fwrite(file.path(out_d, gene, str_replace(basename(in_f), '.tsv$', '.wide.tsv')), sep='\t', na = "NA", quote=F)
+}
